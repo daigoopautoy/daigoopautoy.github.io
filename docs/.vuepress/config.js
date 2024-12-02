6e2d458503a9d7be002a4b57a2fa17a1c87fd4ee
crc <939092353@qq.com>
2024-12-02 23:03:33 +0800

    update workflow

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,6 +3,20 @@ import { defaultTheme } from '@vuepress/theme-default'
 import { defineUserConfig } from 'vuepress'
 
 export default defineUserConfig({
-  bundler: viteBundler(),
-  theme: defaultTheme(),
+    bundler: viteBundler(),
+    theme: defaultTheme({
+        navbar: [
+            {
+                text: '首页',
+                link: '/'
+            },
+            {
+                text: 'Java',
+                link: '/Java/Java.html'
+            }
+        ]
+    }),
+    lang: 'zh-CN',
+    title: '你好， VuePress ！',
+    description: '这是我的第一个 VuePress 站点',
 })
